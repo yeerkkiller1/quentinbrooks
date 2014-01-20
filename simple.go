1a57796a6c17f6ea663a8c75419e02d4b2fda04a
yeerkkiller1 <yeerkkiller1@gmail.com>
2014-01-20 01:10:38 -0500

    BuildBlast stuff

---
+++ simple.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Workaround for Quentin's system configuration.
+	// For some reason, css files are getting served
+	// without a content-type...
+	if strings.HasSuffix(r.URL.Path, ".css") {
+		w.Header().Set("Content-Type", "text/css")
+	}
+	http.ServeFile(w, r, "."+r.URL.Path)
+}
+
+func main() {
+	portNumber := flag.Int("port", 8080, "Sets the port the server listens on for both http requests and websocket connections.")
+
+	flag.Parse()
+
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Beginning HTTP listening on port", *portNumber)
+	err := http.ListenAndServe(":"+strconv.Itoa(*portNumber), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
