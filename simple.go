87ab54f5d0b00c6db6e71e7d4c22a457537b5a46
yeerkkiller1 <yeerkkiller1@gmail.com>
2014-01-19 23:44:15 -0500

    Simple http server.

---
+++ simple.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Workaround for Quentin's system configuration.
+	// For some reason, css files are getting served
+	// without a content-type...
+	if strings.HasSuffix(r.URL.Path, ".css") {
+		w.Header().Set("Content-Type", "text/css")
+	}
+	http.ServeFile(w, r, "."+r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+}
